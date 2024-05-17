fea3212ffbe3d6ce55a7095e31a344471bbb944b
Pratik <kalepg45@gmail.com>
2024-05-16 21:38:27 -0400

    Version upgrades

--- camera.py
+++ camera.py
@@ -21,11 +21,11 @@ class VideoCamera(object):
         faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
 
         for (x, y, w, h) in faces:
-        	cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-        	roi_gray = gray[y:y + h, x:x + w]
-        	cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        	prediction = model.predict_emotion(cropped_img)
-	        cv2.putText(frame, prediction, (x+20, y-60), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict_emotion(cropped_img)
+            cv2.putText(frame, prediction, (x+20, y-60), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
 
         _, jpeg = cv2.imencode('.jpg', cv2.resize(frame,(800,480), interpolation = cv2.INTER_CUBIC))
         return jpeg.tobytes()
--- model.py
+++ model.py
@@ -1,37 +1,45 @@
 import numpy as np
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-class FacialExpressionModel(object):
+class FacialExpressionModel:
     emotion_dict = ["Angry", "Disgusted", "Fearful", "Happy", "Neutral", "Sad", "Surprised"]
 
     def __init__(self, model_weights_file):
-    	self.loaded_model = Sequential()
+        self.loaded_model = Sequential()
+        
+        self.loaded_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+        self.loaded_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+        self.loaded_model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.loaded_model.add(Dropout(0.25))
 
-    	self.loaded_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-    	self.loaded_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    	self.loaded_model.add(MaxPooling2D(pool_size=(2, 2)))
-    	self.loaded_model.add(Dropout(0.25))
+        self.loaded_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.loaded_model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.loaded_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.loaded_model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.loaded_model.add(Dropout(0.25))
 
-    	self.loaded_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    	self.loaded_model.add(MaxPooling2D(pool_size=(2, 2)))
-    	self.loaded_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    	self.loaded_model.add(MaxPooling2D(pool_size=(2, 2)))
-    	self.loaded_model.add(Dropout(0.25))
+        self.loaded_model.add(Flatten())
+        self.loaded_model.add(Dense(1024, activation='relu'))
+        self.loaded_model.add(Dropout(0.5))
+        self.loaded_model.add(Dense(7, activation='softmax'))
 
-    	self.loaded_model.add(Flatten())
-    	self.loaded_model.add(Dense(1024, activation='relu'))
-    	self.loaded_model.add(Dropout(0.5))
-    	self.loaded_model.add(Dense(7, activation='softmax'))
-    	self.loaded_model.load_weights(model_weights_file)
-    	self.loaded_model._make_predict_function()
+        # Load the model weights
+        self.loaded_model.load_weights(model_weights_file)
 
     def predict_emotion(self, img):
-    	self.preds = self.loaded_model.predict(img)
-        return FacialExpressionModel.emotion_dict[int(np.argmax(self.preds))]
+        # Ensure the image is the correct shape
+        if img.shape != (1, 48, 48, 1):
+            raise ValueError("Input image must be of shape (1, 48, 48, 1)")
+
+        # Predict the emotion
+        preds = self.loaded_model.predict(img)
+        return FacialExpressionModel.emotion_dict[int(np.argmax(preds))]
+
+# Example usage (ensure the model_weights_file path is correct):
+# model = FacialExpressionModel('path_to_weights_file.h5')
+# img = np.random.rand(1, 48, 48, 1)  # Example input image
+# emotion = model.predict_emotion(img)
+# print(emotion)
