794491cd360cd72584b78e1a78e57dc3e9c452e0
Pratik <kalepg45@gmail.com>
2026-03-01 15:25:17 -0500

    Add optional HTTPS support with custom domain
    - Support both HTTP (default) and HTTPS (when domain configured)
    - Use ACM certificate with Route53 DNS validation
    - Auto-redirect HTTP to HTTPS when domain is provided
    - Matches pattern used in portfolio project
    - Camera access will work once domain with HTTPS is configured

--- infrastructure/bin/app.ts
+++ infrastructure/bin/app.ts
@@ -4,9 +4,14 @@ import { EmotionPredictionStack } from '../lib/emotion-prediction-stack';
 
 const app = new cdk.App();
 
+const domainName = process.env.DOMAIN_NAME;
+const hostedZoneId = process.env.HOSTED_ZONE_ID;
+
 new EmotionPredictionStack(app, 'EmotionPredictionStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
   },
+  domainName,
+  hostedZoneId,
 });
--- infrastructure/lib/emotion-prediction-stack.ts
+++ infrastructure/lib/emotion-prediction-stack.ts
@@ -1,13 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
-import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
 import { Construct } from 'constructs';
 import * as path from 'path';
 
+export interface EmotionPredictionStackProps extends cdk.StackProps {
+  domainName?: string;
+  hostedZoneId?: string;
+}
+
 export class EmotionPredictionStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: EmotionPredictionStackProps) {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, 'EmotionVpc', {
@@ -24,32 +31,105 @@ export class EmotionPredictionStack extends cdk.Stack {
       directory: path.join(__dirname, '../../'),
     });
 
-    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
-      this,
-      'EmotionService',
-      {
-        cluster,
-        cpu: 512,
-        memoryLimitMiB: 1024,
-        desiredCount: 1,
-        taskImageOptions: {
-          image: ecs.ContainerImage.fromDockerImageAsset(image),
-          containerPort: 8080,
-        },
-        publicLoadBalancer: true,
-      }
-    );
-
-    fargateService.targetGroup.configureHealthCheck({
-      path: '/',
-      interval: cdk.Duration.seconds(60),
-      timeout: cdk.Duration.seconds(30),
-      healthyThresholdCount: 2,
-      unhealthyThresholdCount: 3,
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'EmotionTask', {
+      memoryLimitMiB: 1024,
+      cpu: 512,
     });
 
+    const container = taskDefinition.addContainer('EmotionContainer', {
+      image: ecs.ContainerImage.fromDockerImageAsset(image),
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'emotion' }),
+    });
+
+    container.addPortMappings({ containerPort: 8080 });
+
+    const service = new ecs.FargateService(this, 'EmotionService', {
+      cluster,
+      taskDefinition,
+      desiredCount: 1,
+    });
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'EmotionALB', {
+      vpc,
+      internetFacing: true,
+    });
+
+    const httpListener = alb.addListener('HttpListener', { port: 80 });
+
+    const targetGroup = new elbv2.ApplicationTargetGroup(this, 'EmotionTarget', {
+      vpc,
+      port: 8080,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      targets: [service],
+      healthCheck: {
+        path: '/',
+        interval: cdk.Duration.seconds(60),
+        timeout: cdk.Duration.seconds(30),
+        healthyThresholdCount: 2,
+        unhealthyThresholdCount: 3,
+      },
+    });
+
+    // If domain and hosted zone provided, add HTTPS
+    if (props?.domainName && props?.hostedZoneId) {
+      const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+        hostedZoneId: props.hostedZoneId,
+        zoneName: props.domainName,
+      });
+
+      const certificate = new acm.Certificate(this, 'Certificate', {
+        domainName: props.domainName,
+        validation: acm.CertificateValidation.fromDns(hostedZone),
+      });
+
+      // HTTPS listener
+      const httpsListener = alb.addListener('HttpsListener', {
+        port: 443,
+        certificates: [certificate],
+      });
+
+      httpsListener.addTargetGroups('HttpsTargetGroup', {
+        targetGroups: [targetGroup],
+      });
+
+      // Redirect HTTP to HTTPS
+      httpListener.addAction('RedirectToHttps', {
+        action: elbv2.ListenerAction.redirect({
+          protocol: 'HTTPS',
+          port: '443',
+          permanent: true,
+        }),
+      });
+
+      // Create Route53 A record
+      new route53.ARecord(this, 'AliasRecord', {
+        zone: hostedZone,
+        target: route53.RecordTarget.fromAlias({
+          bind: () => ({
+            dnsName: alb.loadBalancerDnsName,
+            hostedZoneId: alb.loadBalancerCanonicalHostedZoneId,
+          }),
+        }),
+      });
+
+      new cdk.CfnOutput(this, 'DomainURL', {
+        value: `https://${props.domainName}`,
+        description: 'Application URL',
+      });
+    } else {
+      // No HTTPS, just forward HTTP traffic
+      httpListener.addTargetGroups('HttpTargetGroup', {
+        targetGroups: [targetGroup],
+      });
+
+      new cdk.CfnOutput(this, 'LoadBalancerURL', {
+        value: `http://${alb.loadBalancerDnsName}`,
+        description: 'Application URL (HTTP only - HTTPS requires domain)',
+      });
+    }
+
     new cdk.CfnOutput(this, 'LoadBalancerDNS', {
-      value: fargateService.loadBalancer.loadBalancerDnsName,
+      value: alb.loadBalancerDnsName,
       description: 'Load Balancer DNS',
     });
   }
